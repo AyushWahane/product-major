ce36839798aa6720bf71d7655151cc0abda2382b
AyushWahane <ayushwahane007@gmail.com>
2023-05-18 14:42:13 +0530

    changes updtae

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker login -u krishna1708 -p Abcd97@4321
-docker rmi krishna1708/product-major:latest 
-docker build -t krishna1708/product-major:latest .
-docker push krishna1708/product-major:latest
+docker rmi krishna1708/product-major:v1 
+docker build -t krishna1708/product-major:v1 .
+docker push krishna1708/product-major:v1
